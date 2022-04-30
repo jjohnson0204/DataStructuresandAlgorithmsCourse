8a7bb593e1605b749eb5d145172c0aeabcb3cbd2
jjohnson0204 <joejohnson0204@gmail.com>
2022-04-30 18:31:55 -0400

    Initial commit

---
+++ src/Stacks/Stacks.java
@@ -0,0 +1,15 @@
+package Stacks;
+
+public class Stacks {
+
+    public static void main(String[] args) {
+
+        /* stack = LIFO data structure
+                stores objects into a sort of "vertical tower"
+                push() to add to the top
+                pop() to remove from the top
+         */
+
+
+    }
+}
