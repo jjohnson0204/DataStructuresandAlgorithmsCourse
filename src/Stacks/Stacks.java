120f53a8ab66c9d654e2d0ee8af4a79b6a20fe54
jjohnson0204 <joejohnson0204@gmail.com>
2022-04-30 18:45:43 -0400

    Update to Stacks.java

--- src/Stacks/Stacks.java
+++ src/Stacks/Stacks.java
@@ -1,5 +1,7 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class Stacks {
 
     public static void main(String[] args) {
@@ -9,6 +11,36 @@ public class Stacks {
                 push() to add to the top
                 pop() to remove from the top
          */
+
+        Stack<String> stack=new Stack<String>();
+
+        stack.push("Genshin Impact");
+        stack.push("Fortnite");
+        stack.push("Apex");
+        stack.push("Smite");
+        stack.push("Destiny 2");
+        stack.push("Paladins");
+        stack.push("Final Fantasy XV");
+
+        String myFavGame = stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack.search("Genshin Impact"));
+        System.out.println(stack);
+        System.out.println(myFavGame);
+
+        /*
+            *************************output****************************
+            Paladins
+            6
+            [Genshin Impact, Forntie, Apex, Smite, Destiny 2, Paladins]
+            Final Fantasy XV
+
+            Process finished with exit code 0
+            ***********************************************************
+         */
+
+
+
 
 
     }
